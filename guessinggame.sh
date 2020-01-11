b185ea36bc7da16d81e4d72f091481df34871a15
divyabedi1 <divya.bedi0@gmail.com>
2020-01-11 19:05:59 +0530

    added new files

---
+++ guessinggame.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+function guessing_game(){
+    local direc=$(ls -l | grep -v ^l | wc -l)
+    while [[1=1]]
+    do
+        echo "Try to guess the number of files in the directory?"
+        read number
+		if [[ "$number" =~ ^[0-9]+$ ]]
+		then
+			if [ $number -lt $direc ]
+			then
+				echo "Your guess is Lesser than the actual number."
+			elif [ $number -gt $direc ]
+			then
+				echo "Your guess is Greater than the actual number."
+			else
+				echo "Congratulations, You are Right!"
+				break
+			fi
+        fi
+    done
+}
+echo "Guess the number of files in the current directory :)"
+guessing_game
+
